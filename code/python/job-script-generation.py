e14e462230019d2ddaee46b7b4b7eef0a2639a70
Noah Prime <noahprime21@gmail.com>
2023-10-03 16:03:35 -0400

    Updated job script generation for tasmin/tasmax

--- code/python/job-script-generation.py
+++ code/python/job-script-generation.py
@@ -19,125 +19,210 @@ import sys
 import numpy as np
 import pandas as pd
 
-# Define paths
-input_files_path = 'input'
-intermediate_path = 'intermediate'
-# TODO: Allow for this to be an argument passed in
-run_manager_file = str(sys.argv[1])
-# run_manager_file = 'run_manager.csv'
+if __name__ == "__main__":
 
-# Helper function to remove nans
-def remove_nas(x):
-    return x[~pd.isnull(x)]
+    # Read in desired run
+    run_name = str(sys.argv[1])
 
-# Read in user defined job requests
-run_manager_df = pd.read_csv(os.path.join(input_files_path, run_manager_file))
-esms = remove_nas(run_manager_df['ESM'].values)
-esm_input_paths = remove_nas(run_manager_df['ESM_Input_Location'].values)
-output_paths = remove_nas(run_manager_df['Output_Location'].values)
-ref_datasets = remove_nas(run_manager_df['Reference_Dataset'].values)
-ref_datasets_paths = remove_nas(run_manager_df['Reference_Input_Location'].values)
-variables = remove_nas(run_manager_df['Variable'].values)
-scenarios = remove_nas(run_manager_df['Scenario'].values)
-ensembles = remove_nas(run_manager_df['Ensemble'].values)
-target_periods = remove_nas(run_manager_df['target_period'].values)
-application_periods = remove_nas(run_manager_df['application_period'].values)
-daily = remove_nas(run_manager_df['daily'].values)
-monthly = remove_nas(run_manager_df['monthly'].values)
-stitched = remove_nas(run_manager_df['stitched'].values)
+    # Define paths
+    input_files_path = 'input'
+    intermediate_path = 'intermediate'
+    run_manager_file = 'run_manager.csv'
 
-# If you want to use BASD for tasmin or tasmax, need to use tas, tasrange and tasskew to do so indirectly
-# So here we make sure to have those variables present when tasmax and/or tasmin is present,
-# and remove the direct tasmin/tasmax calls. We will create these later from the results
-if ('tasmax' in variables) or ('tasmin' in variables):
-    variables = np.union1d(np.setdiff1d(variables, ['tasmax', 'tasmin']), ['tas', 'tasrange', 'tasskew'])
+    # Helper function to remove nans
+    def remove_nas(x):
+        return x[~pd.isnull(x)]
 
-# If no ensembles given (this happens when we're using STITCHED data)
-if len(ensembles) == 0:
-    # Get all combinations (as an array, each row represents a single job)
-    mesh_array = np.array(np.meshgrid(esms, 
+    # Read in user defined job requests
+    run_manager_df = pd.read_csv(os.path.join(input_files_path, run_name, run_manager_file))
+    esms = remove_nas(run_manager_df['ESM'].values)
+    esm_input_paths = remove_nas(run_manager_df['ESM_Input_Location'].values)
+    output_paths = remove_nas(run_manager_df['Output_Location'].values)
+    ref_datasets = remove_nas(run_manager_df['Reference_Dataset'].values)
+    ref_datasets_paths = remove_nas(run_manager_df['Reference_Input_Location'].values)
+    variables = remove_nas(run_manager_df['Variable'].values)
+    scenarios = remove_nas(run_manager_df['Scenario'].values)
+    ensembles = remove_nas(run_manager_df['Ensemble'].values)
+    target_periods = remove_nas(run_manager_df['target_period'].values)
+    application_periods = remove_nas(run_manager_df['application_period'].values)
+    daily = remove_nas(run_manager_df['daily'].values)
+    monthly = remove_nas(run_manager_df['monthly'].values)
+    stitched = remove_nas(run_manager_df['stitched'].values)
+
+    # If you want to use BASD for tasmin or tasmax, need to use tas, tasrange and tasskew to do so indirectly
+    # So here we make sure to have those variables present when tasmax and/or tasmin is present,
+    # and remove the direct tasmin/tasmax calls. We will create these later from the results
+    if ('tasmax' in variables) or ('tasmin' in variables):
+        variables = np.union1d(np.setdiff1d(variables, ['tasmax', 'tasmin']), ['tas', 'tasrange', 'tasskew'])
+
+    # If no ensembles given (this happens when we're using STITCHED data)
+    if len(ensembles) == 0:
+        # Get all combinations (as an array, each row represents a single job)
+        mesh_array = np.array(np.meshgrid(esms, 
+                                        variables, 
+                                        scenarios, 
+                                        ref_datasets, 
+                                        target_periods, 
+                                        application_periods)).T.reshape(-1,6)
+        # Convert to pandas DataFrame and add back in columns that didn't need extra enumeration
+        mesh_df = pd.DataFrame(mesh_array, columns = ['ESM', 'Variable', 'Scenario', 'Reference_Dataset',
+                                                'target_period', 'application_period'])
+    # When using ensemble members (using standard CMIP data)
+    else:
+        # Get all combinations (as an array, each row represents a single job)
+        mesh_array = np.array(np.meshgrid(esms, 
                                     variables, 
                                     scenarios, 
+                                    ensembles,
                                     ref_datasets, 
                                     target_periods, 
-                                    application_periods)).T.reshape(-1,6)
-    # Convert to pandas DataFrame and add back in columns that didn't need extra enumeration
-    mesh_df = pd.DataFrame(mesh_array, columns = ['ESM', 'Variable', 'Scenario', 'Reference_Dataset',
-                                              'target_period', 'application_period'])
-# When using ensemble members (using standard CMIP data)
-else:
-    # Get all combinations (as an array, each row represents a single job)
-    mesh_array = np.array(np.meshgrid(esms, 
-                                  variables, 
-                                  scenarios, 
-                                  ensembles,
-                                  ref_datasets, 
-                                  target_periods, 
-                                  application_periods)).T.reshape(-1,7)
-    # Convert to pandas DataFrame and add back in columns that didn't need extra enumeration
-    mesh_df = pd.DataFrame(mesh_array, columns = ['ESM', 'Variable', 'Scenario', 'Ensemble', 'Reference_Dataset',
-                                              'target_period', 'application_period'])
+                                    application_periods)).T.reshape(-1,7)
+        # Convert to pandas DataFrame and add back in columns that didn't need extra enumeration
+        mesh_df = pd.DataFrame(mesh_array, columns = ['ESM', 'Variable', 'Scenario', 'Ensemble', 'Reference_Dataset',
+                                                'target_period', 'application_period'])
 
 
-# Merge in esm input locations
-mesh_df = mesh_df.merge(run_manager_df[['ESM', 'ESM_Input_Location']], on='ESM', how='inner')
-# Merge in reference dataset input locations
-mesh_df = mesh_df.merge(run_manager_df[['Reference_Dataset', 'Reference_Input_Location']], on='Reference_Dataset', how='inner')
-# Merge in output paths
-mesh_df = mesh_df.merge(run_manager_df[['ESM', 'Output_Location']], on='ESM', how='inner')
-# Add daily and monthly bools
-mesh_df['daily'] = daily[0]
-mesh_df['monthly'] = monthly[0]
-mesh_df['stitched'] = stitched[0]
+    # Merge in esm input locations
+    mesh_df = mesh_df.merge(run_manager_df[['ESM', 'ESM_Input_Location']], on='ESM', how='inner')
+    # Merge in reference dataset input locations
+    mesh_df = mesh_df.merge(run_manager_df[['Reference_Dataset', 'Reference_Input_Location']], on='Reference_Dataset', how='inner')
+    # Merge in output paths
+    mesh_df = mesh_df.merge(run_manager_df[['ESM', 'Output_Location']], on='ESM', how='inner')
+    # Add daily and monthly bools
+    mesh_df['daily'] = daily[0]
+    mesh_df['monthly'] = monthly[0]
+    mesh_df['stitched'] = stitched[0]
 
-# Get file name information for this run request
-file_name = os.path.splitext(run_manager_file)[0]
-job_file_name = f'{file_name}.job'
-out_file_name = f'{file_name}.out'
 
-# Save dataframe of every run to csv
-mesh_df.to_csv(os.path.join(intermediate_path, f'{file_name}_explicit_list.csv'), index=False)
+    # Make new directory if not already created
+    os.makedirs(os.path.join(intermediate_path, run_name), exist_ok=True)
 
-# Read in parameters relating to slurm
-slurm_params = pd.read_csv(os.path.join(input_files_path, 'slurm_parameters.csv'))
-account = slurm_params[slurm_params['parameter'] == 'account']['value'].values[0]
-time = slurm_params[slurm_params['parameter'] == 'time']['value'].values[0]
-partition = slurm_params[slurm_params['parameter'] == 'partition']['value'].values[0]
-max_concurrent = slurm_params[slurm_params['parameter'] == 'max_concurrent']['value'].values[0]
-email = slurm_params[slurm_params['parameter'] == 'email']['value'].values[0]
-mail_type = slurm_params[slurm_params['parameter'] == 'mail-type']['value'].values[0]
+    # Save dataframe of every run to csv
+    mesh_df.to_csv(os.path.join(intermediate_path, run_name, f'run_manager_explicit_list.csv'), index=False)
 
-# Create bash file for submitting all jobs through the slurm scheduler
-# TODO: Add every #SBATCH option provided, and none that aren't
-with open(os.path.join(intermediate_path, job_file_name), 'w') as job_file:
-    job_file.writelines(f"#!/bin/bash\n\n")
-    job_file.writelines(f"#=================================================\n")
-    job_file.writelines(f"# This is a bash script for submitting your jobs to the slurm scheduler.\n")
-    job_file.writelines(f"# Feel free to edit this file as you see fit.\n")
-    job_file.writelines(f"# To run, navigate to the root directory of the repo and run:\n")
-    job_file.writelines(f"#     sbatch intermediate/{job_file_name}\n")
-    job_file.writelines(f"# Log file(s) will be created in the hidden .out/ directory called {file_name}_<job_number>.out\n")
-    job_file.writelines(f"#=================================================\n\n")
-    job_file.writelines('# Slurm Settings\n')
-    job_file.writelines(f"#SBATCH --account={account}\n")
-    job_file.writelines(f"#SBATCH --partition={partition}\n")
-    job_file.writelines(f"#SBATCH --job-name={file_name}\n")
-    job_file.writelines(f"#SBATCH --time={time}\n")
-    job_file.writelines(f"#SBATCH --mail-type={mail_type}\n")
-    job_file.writelines(f"#SBATCH --mail-user={email}\n")
-    job_file.writelines(f"#SBATCH --output=\".out/%x_%j.out\"\n")
-    job_file.writelines(f"#SBATCH --array=0-{mesh_df.shape[0]-1}%{max_concurrent}\n\n\n")
-    job_file.writelines('# Load Modules\n')
-    job_file.writelines('module load gcc/11.2.0\n')
-    job_file.writelines('module load python/miniconda3.9\n')
-    job_file.writelines('source /share/apps/python/miniconda3.9/etc/profile.d/conda.sh\n\n')
-    job_file.writelines('# activate conda environment\n')
-    job_file.writelines('conda activate basd_env\n\n')
-    job_file.writelines('# Timing\n')
-    job_file.writelines('start=`date +%s.%N`\n\n')
-    job_file.writelines('# Run script\n')
-    job_file.writelines(f"python code/python/main.py $SLURM_ARRAY_TASK_ID {file_name}_explicit_list.csv \n\n")
-    job_file.writelines('# End timing and print runtime\n')
-    job_file.writelines('end=`date +%s.$N`\n')
-    job_file.writelines('runtime=$( echo "($end - $start) / 60" | bc -l )\n')
-    job_file.writelines('echo "Run completed in $runtime minutes"\n')
+    # Read in parameters relating to slurm
+    slurm_params = pd.read_csv(os.path.join(input_files_path, run_name, 'slurm_parameters.csv'))
+    account = slurm_params[slurm_params['parameter'] == 'account']['value'].values[0]
+    time = slurm_params[slurm_params['parameter'] == 'time']['value'].values[0]
+    partition = slurm_params[slurm_params['parameter'] == 'partition']['value'].values[0]
+    max_concurrent = slurm_params[slurm_params['parameter'] == 'max_concurrent']['value'].values[0]
+    email = slurm_params[slurm_params['parameter'] == 'email']['value'].values[0]
+    mail_type = slurm_params[slurm_params['parameter'] == 'mail-type']['value'].values[0]
+    conda_env = slurm_params[slurm_params['parameter'] == 'conda_env']['value'].values[0]
+
+    # Create bash file for submitting all BASD jobs to slurm
+    with open(os.path.join(intermediate_path, run_name, 'basd.job'), 'w') as job_file:
+        job_file.writelines(f"#!/bin/bash\n\n\n")
+        job_file.writelines('# Slurm Settings\n')
+        job_file.writelines(f"#SBATCH --account={account}\n")
+        job_file.writelines(f"#SBATCH --partition={partition}\n")
+        job_file.writelines(f"#SBATCH --job-name={run_name}_BASD.job\n")
+        job_file.writelines(f"#SBATCH --time={time}\n")
+        job_file.writelines(f"#SBATCH --mail-type={mail_type}\n")
+        job_file.writelines(f"#SBATCH --mail-user={email}\n")
+        job_file.writelines(f"#SBATCH --output=.out/{run_name}_BASD.out\n")
+        job_file.writelines(f"#SBATCH --array=0-{mesh_df.shape[0]-1}%{max_concurrent}\n\n\n")
+        job_file.writelines('# Load Modules\n')
+        job_file.writelines('module load gcc/11.2.0\n')
+        job_file.writelines('module load python/miniconda3.9\n')
+        job_file.writelines('source /share/apps/python/miniconda3.9/etc/profile.d/conda.sh\n\n')
+        job_file.writelines('# activate conda environment\n')
+        job_file.writelines(f'conda activate {conda_env}\n\n')
+        job_file.writelines('# Timing\n')
+        job_file.writelines('start=`date +%s.%N`\n\n')
+        job_file.writelines('# Run script\n')
+        job_file.writelines(f"python ../python/main.py $SLURM_ARRAY_TASK_ID {run_name}\n\n")
+        job_file.writelines('# End timing and print runtime\n')
+        job_file.writelines('end=`date +%s.$N`\n')
+        job_file.writelines('runtime=$( echo "($end - $start) / 60" | bc -l )\n')
+        job_file.writelines('echo "Run completed in $runtime minutes"\n')
+
+    # Create bash file for generating tasrange and tasskew files
+    with open(os.path.join(intermediate_path, run_name, 'tasrange_tasskew.job'), 'w') as job_file:
+        job_file.writelines(f"#!/bin/bash\n\n\n")
+        job_file.writelines('# Slurm Settings\n')
+        job_file.writelines(f"#SBATCH --account={account}\n")
+        job_file.writelines(f"#SBATCH --partition={partition}\n")
+        job_file.writelines(f"#SBATCH --job-name={run_name}_tasrange_tasskew.job\n")
+        job_file.writelines(f"#SBATCH --time={time}\n")
+        job_file.writelines(f"#SBATCH --mail-type={mail_type}\n")
+        job_file.writelines(f"#SBATCH --mail-user={email}\n")
+        job_file.writelines(f"#SBATCH --output=.out/{run_name}_tasrange_tasskew.out\n\n\n")
+        job_file.writelines('# Load Modules\n')
+        job_file.writelines('module load gcc/11.2.0\n')
+        job_file.writelines('module load python/miniconda3.9\n')
+        job_file.writelines('source /share/apps/python/miniconda3.9/etc/profile.d/conda.sh\n\n')
+        job_file.writelines('# activate conda environment\n')
+        job_file.writelines(f'conda activate {conda_env}\n\n')
+        job_file.writelines('# Timing\n')
+        job_file.writelines('start=`date +%s.%N`\n\n')
+        job_file.writelines('# Run script\n')
+        job_file.writelines(f"python ../python/create_tasrange_tasskew.py {run_name}\n\n")
+        job_file.writelines('# End timing and print runtime\n')
+        job_file.writelines('end=`date +%s.$N`\n')
+        job_file.writelines('runtime=$( echo "($end - $start) / 60" | bc -l )\n')
+        job_file.writelines('echo "Run completed in $runtime minutes"\n')
+
+    # Create bash file for generating tasmin and tasmax files
+    with open(os.path.join(intermediate_path, run_name, 'tasmin_tasmax.job'), 'w') as job_file:
+        job_file.writelines(f"#!/bin/bash\n\n\n")
+        job_file.writelines('# Slurm Settings\n')
+        job_file.writelines(f"#SBATCH --account={account}\n")
+        job_file.writelines(f"#SBATCH --partition={partition}\n")
+        job_file.writelines(f"#SBATCH --job-name={run_name}_tasmin_tasmax.job\n")
+        job_file.writelines(f"#SBATCH --time={time}\n")
+        job_file.writelines(f"#SBATCH --mail-type={mail_type}\n")
+        job_file.writelines(f"#SBATCH --mail-user={email}\n")
+        job_file.writelines(f"#SBATCH --output=.out/{run_name}_tasmin_tasmax.out\n\n\n")
+        job_file.writelines('# Load Modules\n')
+        job_file.writelines('module load gcc/11.2.0\n')
+        job_file.writelines('module load python/miniconda3.9\n')
+        job_file.writelines('source /share/apps/python/miniconda3.9/etc/profile.d/conda.sh\n\n')
+        job_file.writelines('# activate conda environment\n')
+        job_file.writelines(f'conda activate {conda_env}\n\n')
+        job_file.writelines('# Timing\n')
+        job_file.writelines('start=`date +%s.%N`\n\n')
+        job_file.writelines('# Run script\n')
+        job_file.writelines(f"python ../python/create_tasmin_tasmax.py {run_name}\n\n")
+        job_file.writelines('# End timing and print runtime\n')
+        job_file.writelines('end=`date +%s.$N`\n')
+        job_file.writelines('runtime=$( echo "($end - $start) / 60" | bc -l )\n')
+        job_file.writelines('echo "Run completed in $runtime minutes"\n')
+
+    # Create bash file for managing the above files in order
+    with open(os.path.join(intermediate_path, run_name, 'manager.job'), 'w') as job_file:
+        job_file.writelines(f"#!/bin/bash\n\n\n")
+
+        job_file.writelines('# Slurm Settings\n')
+        job_file.writelines(f"#SBATCH --account={account}\n")
+        job_file.writelines(f"#SBATCH --partition={partition}\n")
+        job_file.writelines(f"#SBATCH --job-name={run_name}_tasmin_tasmax.job\n")
+        job_file.writelines(f"#SBATCH --time={time}\n")
+        job_file.writelines(f"#SBATCH --mail-type={mail_type}\n")
+        job_file.writelines(f"#SBATCH --mail-user={email}\n")
+        job_file.writelines(f"#SBATCH --output=.out/{run_name}_tasmin_tasmax.out\n\n\n")
+
+        job_file.writelines('# Load Modules\n')
+        job_file.writelines('module load gcc/11.2.0\n')
+        job_file.writelines('module load python/miniconda3.9\n')
+        job_file.writelines('source /share/apps/python/miniconda3.9/etc/profile.d/conda.sh\n\n')
+
+        job_file.writelines('# activate conda environment\n')
+        job_file.writelines(f'conda activate {conda_env}\n\n')
+
+        job_file.writelines('# Timing\n')
+        job_file.writelines('start=`date +%s.%N`\n\n')
+
+        job_file.writelines('# Run tasrange and tasskew creation job\n')
+        job_file.writelines("range_skew_id=$(sbatch --parsable tasrange-tasskew.job)\n\n")
+
+        job_file.writelines('# Run bias adjustment and downscaling\n')
+        job_file.writelines("basd_id=$(sbatch --parsable --dependency=afterok:$range_skew_id basd.job)\n\n")
+
+        job_file.writelines('# Run tasmin and tasmax creation job\n')
+        job_file.writelines("min_max_id=$(sbatch --parsable --dependency=afterok:$basd_id basd.job)\n\n")
+
+        job_file.writelines('# End timing and print runtime\n')
+        job_file.writelines('end=`date +%s.$N`\n')
+        job_file.writelines('runtime=$( echo "($end - $start) / 60" | bc -l )\n')
+        job_file.writelines('echo "Run completed in $runtime minutes"\n')
